656c226513218ab6dfbf0168fd55732620278769
Ryan Prince <ryanprince@macbook-pro.myfiosgateway.com>
2022-10-12 18:01:55 -0400

    first commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Ryan Prince on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
